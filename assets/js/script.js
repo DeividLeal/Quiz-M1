e2337a160bb0aca24f219b7bde080fd89daad036
Graziele <grazziele2701@gmail.com>
2024-09-28 13:23:20 -0300

    Atualizando perguntas mock

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,41 +4,75 @@ let timerInterval; // Intervalo do cronômetro
 let score = 0; // Variável para armazenar o contador de pontos
 let currentQuestionIndex = 0;
 
-// Array de perguntas e respostas
-const questions = [
+// / Array de perguntas e respostas
+const allQuestions = [
     {
-        textPerson: "Qual princesa foi inspirada em uma figura histórica real, uma nativa americana?", // Audio Personagem
-        options: ['Pocahontas', 'Cinderela', 'Moana', 'Mulan'],
+        question: "Qual princesa foi inspirada em uma figura histórica real, uma nativa americana?",
+        options: [
+            { text: 'Pocahontas', image: './assets/img/Pocahontas-face.jpeg' },
+            { text: 'Jasmine', image: './assets/img/Jasmine-face.webp' },
+            { text: 'Mulan', image: './assets/img/Mulan-face.jpeg' },
+            { text: 'Tiana', image: './assets/img/Tiana-face.jpg' }
+        ],
         correct: 0
     },
-    
     {
-        textPerson: 'Qual princesa é filha do chefe de uma tribo e sonha em navegar pelo oceano?', // Audio Personagem
-        options: ['Tiana', 'Cinderela', 'Moana', 'Jasmine'],
+        question: 'Qual princesa é filha do chefe de uma tribo e sonha em navegar pelo oceano?',
+        options: [
+            { text: 'Tiana', image: './assets/img/Tiana-face.jpg' },
+            { text: 'Cinderela', image: './assets/img/Cinderela-face.png' },
+            { text: 'Moana', image: './assets/img/Moana-face.jpg' },
+            { text: 'Jasmine', image: './assets/img/Jasmine-face.webp' }
+        ],
         correct: 2
     },
     {
-        textPerson: 'Qual princesa foi criada por sua madrasta e é famosa por seu sapatinho de cristal?', // Audio Personagem
-        options: ['Jasmine', ' Mulan', 'Cinderela', 'Tiana'],
+        question: 'Qual princesa foi criada por sua madrasta e é famosa por seu sapatinho de cristal?',
+        options: [
+            { text: 'Jasmine', image: './assets/img/Jasmine-face.webp' },
+            { text: 'Mulan', image: './assets/img/Mulan-face.jpeg' },
+            { text: 'Cinderela', image: './assets/img/Cinderela-face.png' },
+            { text: 'Tiana', image: './assets/img/Tiana-face.jpg' }
+        ],
         correct: 2
-    },  
+    },
     {
-        textPerson: 'Qual princesa se disfarça de guerreiro para salvar seu país da invasão dos Hunos?', // Audio Personagem
-        options: ['Moana', 'Mulan', 'Jasmine', 'Tiana'],
+        question: 'Qual princesa se disfarça de guerreiro para salvar seu país da invasão dos Hunos?',
+        options: [
+            { text: 'Moana', image: './assets/img/Moana-face.jpg' },
+            { text: 'Mulan', image: './assets/img/Mulan-face.jpeg' },
+            { text: 'Jasmine', image: './assets/img/Jasmine-face.webp' },
+            { text: 'Tiana', image: './assets/img/Tiana-face.jpg' }
+        ],
         correct: 1
     },
-    
     {
-        textPerson: 'Qual princesa vive em Agrabah e tem um tapete mágico?', // Audio Personagem
-        options: ['Cinderela', 'Pocahontas', 'Jasmine', ' Mulan'],
+        question: 'Qual princesa vive em Agrabah e tem um tapete mágico?',
+        options: [
+            { text: 'Cinderela', image: './assets/img/Cinderela-face.png' },
+            { text: 'Pocahontas', image: './assets/img//Pocahontas-face.jpeg' },
+            { text: 'Jasmine', image: './assets/img/Jasmine-face.webp' },
+            { text: 'Mulan', image: './assets/img/Mulan-face.jpeg' }
+        ],
         correct: 2
     },
     {
-        textPerson: 'Qual princesa sonha em abrir seu próprio restaurante em Nova Orleans?', // Audio Personagem
-        options: ['Tiana ', 'Pocahontas', 'Moana', ' Jasmine'],
+        question: 'Qual princesa sonha em abrir seu próprio restaurante em Nova Orleans?',
+        options: [
+            { text: 'Tiana', image: './assets/img/Tiana-face.jpg' },
+            { text: 'Pocahontas', image: './assets/img//Pocahontas-face.jpeg' },
+            { text: 'Moana', image: './assets/img/Moana-face.jpg' },
+            { text: 'Jasmine', image: './assets/img/Jasmine-face.webp' }
+        ],
         correct: 0
-    }, 
+    },
+
 ];
+
+let username = ''; // Variável para armazenar o nome do usuário
+let currentQuestionIndex = 0; // Índice da pergunta atual
+let totalQuestions = allQuestions.length; // Quantidade total de perguntas
+let points = 0; // Variável para armazenar os pontos, começa em 1
 
 // Função para iniciar o jogo e fechar a modal
 function startGame() {
